3d24c80ea092fe0e5f9506fa72331da344f582f6
JonathanGio <jonathangio.ma@codexcityinc.com.mx>
2018-05-29 12:18:32 -0500

    Deploy heroku

---
+++ app/Http/Controllers/PaypalPaymentController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Paypalpayment;
+
+class PaypalPaymentController extends Controller
+{
+    //
+    public function paywithCreditCard()
+    {
+        // ### Address
+        // Base Address object used as shipping or billing
+        // address in a payment. [Optional]
+        $shippingAddress = Paypalpayment::shippingAddress();
+        $shippingAddress->setLine1("3909 Geovillas de Independencia")
+            ->setLine2("Toluca, Edo de Méx")
+            ->setCity("Toluca, Edo de Méx")
+            ->setState("MX")
+            ->setPostalCode("51370")
+            ->setCountryCode("US")
+            ->setPhone("722-5-28-24-79")
+            ->setRecipientName("Juan Carlos Garcia");
+
+        // ### CreditCard
+        $card = Paypalpayment::creditCard();
+        $card->setType("visa")
+            ->setNumber("4758411877817150")
+            ->setExpireMonth("05")
+            ->setExpireYear("2019")
+            ->setCvv2("456")
+            ->setFirstName("Joe")
+            ->setLastName("Shopper");
+
+        // ### FundingInstrument
+        // A resource representing a Payer's funding instrument.
+        // Use a Payer ID (A unique identifier of the payer generated
+        // and provided by the facilitator. This is required when
+        // creating or using a tokenized funding instrument)
+        // and the `CreditCardDetails`
+        $fi = Paypalpayment::fundingInstrument();
+        $fi->setCreditCard($card);
+
+        // ### Payer
+        // A resource representing a Payer that funds a payment
+        // Use the List of `FundingInstrument` and the Payment Method
+        // as 'credit_card'
+        $payer = Paypalpayment::payer();
+        $payer->setPaymentMethod("credit_card")
+            ->setFundingInstruments([$fi]);
+
+        $item1 = Paypalpayment::item();
+        $item1->setName('Ground Coffee 40 oz')
+                ->setDescription('Ground Coffee 40 oz')
+                ->setCurrency('USD')
+                ->setQuantity(1)
+                ->setTax(0.05)
+                ->setPrice(0.05);
+
+
+        $itemList = Paypalpayment::itemList();
+        $itemList->setItems([$item1])
+            ->setShippingAddress($shippingAddress);
+
+
+        $details = Paypalpayment::details();
+        $details->setShipping("0.01")
+                ->setTax("0.05")
+                //total of items prices
+                ->setSubtotal("0.05");
+
+        //Payment Amount
+        $amount = Paypalpayment::amount();
+        $amount->setCurrency("USD")
+                // the total is $17.8 = (16 + 0.6) * 1 ( of quantity) + 1.2 ( of Shipping).
+                ->setTotal("0.11")
+                ->setDetails($details);
+
+        // ### Transaction
+        // A transaction defines the contract of a
+        // payment - what is the payment for and who
+        // is fulfilling it. Transaction is created with
+        // a `Payee` and `Amount` types
+
+        $transaction = Paypalpayment::transaction();
+        $transaction->setAmount($amount)
+            ->setItemList($itemList)
+            ->setDescription("Payment description")
+            ->setInvoiceNumber(uniqid());
+
+        // ### Payment
+        // A Payment Resource; create one using
+        // the above types and intent as 'sale'
+
+        $payment = Paypalpayment::payment();
+
+        $payment->setIntent("sale")
+            ->setPayer($payer)
+            ->setTransactions([$transaction]);
+
+        try {
+            // ### Create Payment
+            // Create a payment by posting to the APIService
+            // using a valid ApiContext
+            // The return object contains the status;
+            $payment->create(Paypalpayment::apiContext());
+        } catch (\PPConnectionException $ex) {
+            return response()->json(["error" => $ex->getMessage()], 400);
+        }
+
+        return response()->json([$payment->toArray()], 200);
+    }
+}
